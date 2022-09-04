a5680cbecb97c6b894deed8b7886e2857a378f7d
vkammati <kvk5592007@gmail.com>
2022-09-04 20:22:12 +0530

    Added more functionality

--- waitercaller.py
+++ waitercaller.py
@@ -1,10 +1,69 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
+from flask_login import LoginManager, login_required, login_user, logout_user
+from mockdbhelper import MockDBHelper as DBHelper
+from user import User
+from passwordhelper import PasswordHelper
 
 app = Flask(__name__)
+login_manager = LoginManager(app)
+DB = DBHelper()
+PH = PasswordHelper()
 
 @app.route("/")
 def home():
-    return "Under construction"
+    return render_template("home.html")
+
+@app.route("/dashboard")
+@login_required
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template("account.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    email = request.form.get("email")
+    password = request.form.get("password")
+    print(email)
+    print(password)
+    stored_user = DB.get_user(email)
+
+    if stored_user and PH.validate_password(password,
+        stored_user['salt'], stored_user['hashed']):
+
+        user = User(email)
+        login_user(user)
+        return redirect(url_for('account'))
+
+    return home()
+
+@login_manager.user_loader
+def load_user(user_id, remember=True):
+    user_password = DB.get_user(user_id)
+    if user_password:
+        return User(user_id)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
+
+@app.route("/register", methods=["POST"])
+def register():
+    email = request.form.get("email")
+    pw1 = request.form.get("password")
+    pw2 = request.form.get("password2")
+    if not pw1 == pw2:
+        return redirect(url_for('home'))
+    if DB.get_user(email):
+        return redirect(url_for('home'))
+    salt = PH.get_salt()
+    hashed = PH.get_hash(pw1 + salt)
+    DB.add_user(email, salt, hashed)
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
